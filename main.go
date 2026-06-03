925a928ab65d87ec4dd3a086603199f029a0374c
Gelena Zhuravlyova <roselifemeow@gmail.com>
2026-06-03 13:42:28 +0300

    Removed code from main

--- main.go
+++ main.go
@@ -5,26 +5,5 @@ import (
 )
 
 func main() {
-	var l, r int
-	fmt.Scan(&l, &r)
-	var res int
-	for i := l; i < r+1; i++ {
-		copyNum := i
-		digits := make(map[int]bool)
-		isGood := true
-		for copyNum != 0 {
-			digits[copyNum%10] = true
-			copyNum /= 10
-			if len(digits) > 1 {
-				isGood = false
-				break
-			}
-		}
-		if !isGood {
-			continue
-		} else {
-			res++
-		}
-	}
-	fmt.Println(res)
+	fmt.Println("Hi")
 }
