772c1d64150a42130db9752471dff9e3d28cc97f
Gelena Zhuravlyova <roselifemeow@gmail.com>
2026-07-01 17:04:18 +0300

    Added secretSanta solution (not working)

--- T-Bank/secretSanta.go
+++ T-Bank/secretSanta.go
@@ -1,1 +1,32 @@
 package tbank
+
+import "fmt"
+
+func secretSanta() {
+	var n int
+	fmt.Scan(&n)
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&arr[i])
+	}
+	count := make([]int, n+2)
+	for _, elem := range arr {
+		if elem > 0 && elem < n+1 {
+			count[elem]++
+		}
+	}
+	isFunctional := true
+	for _, elem := range count {
+		if elem != 1 {
+			isFunctional = false
+		}
+	}
+	if isFunctional {
+		fmt.Println("-1, -1")
+		return
+	}
+
+	res := []int{-1, -1}
+
+	fmt.Println(res)
+}
